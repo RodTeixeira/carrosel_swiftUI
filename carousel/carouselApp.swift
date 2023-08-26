28e0d1dc72e99bac1a18611a8dd5cd9053c536b0
RodTeixeira <rodofikis@hotmail.com>
2023-08-26 14:57:18 -0300

    Initial Commit

---
+++ carousel/carouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  carouselApp.swift
+//  carousel
+//
+//  Created by Rodolfo on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct carouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
